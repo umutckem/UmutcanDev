ce4d91a60806326e74e3ec989c3dd0ee4d46bfa6
Umutcan Kemahli <130008707+RagnarLorthbro@users.noreply.github.com>
2025-08-09 19:34:47 +0300

    Son guncelleme V1

--- Core/UmutcanDev.Core/Model/Sergi.cs
+++ Core/UmutcanDev.Core/Model/Sergi.cs
@@ -15,15 +15,12 @@ namespace UmutcanDev.Core.Model
 
         public string? ResimUrl { get; set; }
 
-        // ❗ Bu alan EF Core ile doğrudan desteklenmediği için özel işlem gerekir.
-        // Bu yüzden alternatif string olarak tutulur ve View'da string[]'e ayrılır.
-        public string Teknolojiler { get; set; } = string.Empty; // Örn: "C#, ASP.NET, SQL"
+        public string Teknolojiler { get; set; } = string.Empty; 
 
         public DateTime? OlusturmaTarihi { get; set; }
 
         public string? Durum { get; set; }
 
-        // View'da kullanım için hazır dizi
         public List<string> TeknolojilerListesi =>
             string.IsNullOrWhiteSpace(Teknolojiler)
             ? new List<string>()
